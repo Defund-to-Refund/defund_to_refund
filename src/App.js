4f4aa39be0a7ced089c0b5946245a5cf675e43ab
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-06-14 11:01:57 -0400

    pushing up code for Cierra to grab total percentage (SLIDERS ARE STILL BROKEN IN THIS COMMIT)

--- src/App.js
+++ src/App.js
@@ -5,15 +5,26 @@ import './App.css';
 import Title from './components/title'
 import TotalFunds from './components/totalFunds'
 import SliderBar from './components/sliderBar'
+import SliderBar2 from './components/sliderBar2'
 import { groups } from './dummy_data/groups'
 
 function App() {
   const [ funds, setFunds ] = useState(1000000000)
   const [ percentage, setPercentage] = useState([0, 0, 0, 0])
+  const [ totalPercentage, setTotalPercentage] = useState(0)
 
   const handlePercentage = (e, value) => {
     let index = parseInt(e.target.id, 10)
     setPercentage([...percentage.slice(0, index), value, ...percentage.slice(index+1)])
+    let sum = 0
+    for (let i = 0; i < percentage.length; i++) {
+      if (i !== index) {
+        sum += percentage[i]
+      } else {
+        sum += value
+      }
+    }
+    setTotalPercentage(sum)
   }
 
   const groupSections = groups.map( (group, idx) => {
@@ -22,6 +33,16 @@ function App() {
         <p>{group}: </p>
         <SliderBar handlePercentage={handlePercentage} idx={idx}/>
         <p>Percentage: {percentage[idx]}</p>
+      </div>
+    )
+  })
+
+  const groupSections2 = groups.map( (group, idx) => {
+    return (
+      <div key={idx} style={{display: 'flex'}}>
+        <p>{group}: </p>
+        <SliderBar2 />
+        <p>Percentage: TBD</p>
       </div>
     )
   })
@@ -35,6 +56,10 @@ function App() {
         <TotalFunds funds={funds} />
         <div style={{ display: 'flex', flexDirection: 'column'}}>
           {groupSections}
+        </div>
+        <div>Total Percentage: {totalPercentage}%</div>
+        <div style={{ display: 'flex', flexDirection: 'column'}}>
+          {groupSections2}
         </div>
       </header>
     </div>
