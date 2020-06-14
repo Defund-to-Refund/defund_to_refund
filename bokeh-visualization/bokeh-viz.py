bc1532368dde9749c49ee6795efa8c684bd0e5c0
genvalen <genvalen@protonmail.com>
2020-06-14 04:11:42 -0400

    Update column names, change chart color to Spectral

--- bokeh-visualization/bokeh-viz.py
+++ bokeh-visualization/bokeh-viz.py
@@ -3,24 +3,24 @@ from math import pi
 import pandas as pd
 
 from bokeh.io import output_file, show
-from bokeh.palettes import YlOrBr
+from bokeh.palettes import Spectral
 from bokeh.plotting import figure
 from bokeh.transform import cumsum
 from bokeh.layouts import row
 
-# output_file("pie.html")
+output_file("pie.html")
 
 # imported values
 value = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
 # Data
-x1 = { 'Lgbt Services': value[0] , 'Homelessness': value[1], 'Supporting POC initiatives': value[2], 'Women’s services': value[3],
-    'Orphanages': value[4], 'Public libraries': value[5], 'Sanitation': value[6], 'Public Transportation ': value[7],
-    'Funding Medical Healthcare Professionals': value[8]}
+x1 = { 'LGBTQ Services': value[0] , 'Homeless Services': value[1], 'Cultural Affairs': value[2], 'Women\'s Services': value[3],
+    'Children\'s Services': value[4], 'Public Libraries': value[5], 'Sanitation': value[6], 'Public Transportation ': value[7],
+    'Healthcare Professionals': value[8]}
 
 data = pd.Series(x1).reset_index(name='value').rename(columns={'index':'country'})
 data['angle'] = data['value']/data['value'].sum() * 2*pi
-data['color'] = YlOrBr[len(x1)]
+data['color'] = Spectral[len(x1)]
 
 # Figure 1
 p1 = figure(plot_height=800, title="Total Funds", toolbar_location=None,
@@ -41,7 +41,7 @@ p2 = figure(plot_height=800, title="Impacted People", toolbar_location=None,
 
 
 p2.wedge(x=0, y=1, radius=0.4,
-        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum("angle"),
         line_color="white", fill_color='color', source=data)
 
 p2.axis.axis_label=None
