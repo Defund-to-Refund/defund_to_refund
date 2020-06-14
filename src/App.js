ef9744baccfb290374a459f319acc0fff8e4c402
Imade Osifo <toxsik88@gmail.com>
2020-06-14 16:27:06 -0400

    commented out second group stage and enabled functionality for total percentage

--- src/App.js
+++ src/App.js
@@ -9,39 +9,42 @@ import SliderBar2 from './components/sliderBar2'
 import { groups } from './dummy_data/groups'
 
 function App() {
-  const [ funds, setFunds ] = useState(1000000000)
-  const [ percentage, setPercentage] = useState([0, 0, 0, 0])
-  const [ totalPercentage, setTotalPercentage] = useState(0)
+  const [funds, setFunds] = useState(1000000000)
+  const [percentage, setPercentage] = useState([0, 0, 0, 0])
+  const [totalPercentage, setTotalPercentage] = useState(0)
 
   const handlePercentage = (e, value) => {
     let index = parseInt(e.target.id, 10)
     percentage[index] = value //set percentage array with new percentage at teh index of group chosen by slider
-    
-    setPercentage([...percentage.slice(0, index), value, ...percentage.slice(index+1)])
+
+    setPercentage([...percentage.slice(0, index), value, ...percentage.slice(index + 1)])
     let totalValue = percentage.reduce((result, percent) => result + percent)
     let newFunds = 1000000000 - (1000000000 * (totalValue / 100)) //logic to recalc funds based off percentage lost
     setFunds(newFunds) //set value of funs to new calculated value
+
+    setTotalPercentage(totalValue)
+
   }
 
-  const groupSections = groups.map( (group, idx) => {
+  const groupSections = groups.map((group, idx) => {
     return (
-      <div key={idx} style={{display: 'flex'}}>
+      <div key={idx} style={{ display: 'flex' }}>
         <p>{group}: </p>
-        <SliderBar handlePercentage={handlePercentage} idx={idx}/>
+        <SliderBar handlePercentage={handlePercentage} idx={idx} />
         <p>Percentage: {percentage[idx]}</p>
       </div>
     )
   })
 
-  const groupSections2 = groups.map( (group, idx) => {
-    return (
-      <div key={idx} style={{display: 'flex'}}>
-        <p>{group}: </p>
-        <SliderBar2 />
-        <p>Percentage: TBD</p>
-      </div>
-    )
-  })
+  // const groupSections2 = groups.map( (group, idx) => {
+  //   return (
+  //     <div key={idx} style={{display: 'flex'}}>
+  //       <p>{group}: </p>
+  //       <SliderBar2 />
+  //       <p>Percentage: TBD</p>
+  //     </div>
+  //   )
+  // })
 
   console.log(percentage)
   return (
@@ -50,13 +53,13 @@ function App() {
       <header className="App-header">
         <Title />
         <TotalFunds funds={funds} />
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
           {groupSections}
         </div>
         <div>Total Percentage: {totalPercentage}%</div>
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
+        {/* <div style={{ display: 'flex', flexDirection: 'column'}}>
           {groupSections2}
-        </div>
+        </div> */}
       </header>
     </div>
   );
